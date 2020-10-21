86b95d9afe36c48d2b87b50ece72a2b69b66ff1e
yuriy <donovanyuriy@gmail.com>
2020-10-21 22:33:14 +0300

    first

---
+++ resolvers.js
@@ -0,0 +1,64 @@
+const courseData = [
+    {
+        id: '1',
+        title: 'The complete node js dev veloper course',
+        author: 'Yuriy Puris',
+        description: 'Lear node',
+        topic: 'node js',
+        url: 'https://codingthesmartway.com/courses/nodejs/',
+        voteCount: 0
+    },
+    {
+        id: '2',
+        title: 'The complete angular js dev veloper course',
+        author: 'Yuriy Puris',
+        description: 'Lear angular',
+        topic: 'node js',
+        url: 'https://codingthesmartway.com/courses/nodejs-express-mongodb/',
+        voteCount: 0
+    },
+    {
+        id: '3',
+        title: 'The complete js dev veloper course',
+        author: 'Yuriy Puris',
+        description: 'Lear js',
+        topic: 'node js',
+        url: 'https://codingthesmartway.com/courses/understand-javascript/',
+        voteCount: 0
+    }
+];
+
+const resolvers = {
+    Query: {
+        allCourses: (root, { searchTerm }) => {
+            return courseData;
+        },
+        course: (root, { id }) => {
+            return courseData.filter(course => {
+                return course.id === id;
+            })[0];
+            // return courseModel.findOne({ id: id });
+        }
+    },
+    Mutation: {
+        upvote: (root, { id }) => {
+            const course = courseData.filter(course => {
+                return course.id === id;
+            })[0];
+            course.voteCount++;
+            return course;
+        },
+        downvote: (root, { id }) => {
+            const course = courseData.filter(course => {
+                return course.id === id;
+            })[0];
+            course.voteCount--;
+            return course;
+        },
+        addCourse: (root, { title, author, description, topic, url }) => {
+            return null;
+        }
+    }
+};
+
+export default resolvers;
---
+++ schema.js
@@ -0,0 +1,25 @@
+import { ApolloServer, gql } from 'apollo-server-express';
+import resolvers from './resolvers';
+
+const typeDefs = [`
+    type Course {
+        id: String,
+        title: String,
+        author: String,
+        description: String,
+        topic: String,
+        url: String,
+        voteCount: Int,
+    }
+    type Query {
+        allCourses(searchTerm: String): [Course]
+        course(id: String): Course
+    }
+    type Mutation {
+        addCourse(title: String!, author: String!, description: String, topic: String!, url: String): Course
+        upvote(id: String!): Course
+        downvote(id: String!): Course
+    }
+`];
+
+export default typeDefs;
